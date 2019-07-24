170528eef8f99f5c09d00f7551e02975f980fc0b
Christian Roncal <cjl.roncal@gmail.com>
2019-07-24 19:00:49 -0400

    reverted frames # checking

--- model/Detection/Yolo/yolo_gpu.py
+++ model/Detection/Yolo/yolo_gpu.py
@@ -56,15 +56,16 @@ def detect(inputDir, inputFile, framesDir, outputTxt, outputFolder, conf, nms, c
 
 
     framesPath = os.path.join( inputDir, framesDir)
-    # n_frames = len(os.listdir(framesPath))
-    # print(framesPath, n_frames)
-    # if n_frames < 500:
-    if(thread):
-        thread.signalCanvas("Extracting frames from {}".format(inputFile))
-    create_frames(inputDir, inputFile, framesDir)
-    # else:
-    #     if(thread):
-    #         thread.signalCanvas("Found {} frames in  {}. Delete this folder to re-extract frames".format(n_frames, framesPath))
+    # checks if there are enough frames in framesPath
+    n_frames = len(os.listdir(framesPath))
+    print(framesPath, n_frames)
+    if n_frames < 500:
+        if(thread):
+            thread.signalCanvas("Extracting frames from {}".format(inputFile))
+        create_frames(inputDir, inputFile, framesDir)
+    else:
+        if(thread):
+            thread.signalCanvas("Found {} frames in  {}. Delete this folder to re-extract frames".format(n_frames, framesPath))
 
 
     dataloader = DataLoader(
